f713483e117e52ff60ac823e32df36ff992c83f9
epm0dev <epmancini@live.com>
2021-03-28 15:28:48 -0400

    Create placeholder gradient background

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,8 @@
 <template lang="html">
   <div>
     <Navbar></Navbar>
-    <router-view/>
+    <router-view id="viewport"/>
+    <div id="gradient"></div>
   </div>
 </template>
 
@@ -19,6 +20,21 @@ export default {
 <style lang="scss">
   // Import custom theme
   @import 'assets/theme.scss';
+
+  // Set transparent background for main viewport
+  #viewport {
+    background-color: transparent;
+  }
+
+  // Custom gradient background
+  #gradient {
+    z-index: -1;
+    background-image: linear-gradient(30deg, $purple1, $purple2, $green4, $green3, $blue3, $blue1);
+    height: 3000px;
+    width: 100%;
+    position: absolute;
+    top: 0;
+  }
 
   // Import Bootstrap and BootstrapVue source SCSS files
   @import '~bootstrap/scss/bootstrap.scss';
