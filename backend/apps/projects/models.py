ebf695c04720fd81d16dc1ec41c52bec2239a9b2
epm0dev <epmancini@live.com>
2021-04-09 18:14:59 -0400

    Implement skeleton of project model, move projects app unit tests to `projects/tests/` directory

--- backend/apps/projects/models.py
+++ backend/apps/projects/models.py
@@ -1,3 +1,62 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+from apps.validators import JSONSchemaValidator
+
+
+# TODO Docs
+PROJECT_AUTHORS_FIELD_JSON_SCHEMA = {
+    'schema': 'http://json-schema.org/draft-07/schema#',
+    'title': 'Project Authors',
+    'description': 'A list of authors\' names (as strings) of a project event.',
+    'type': 'array',
+    'properties': {
+        'items': {
+            'type': 'string'
+        }
+    }
+}
+
+
+class Project(models.Model):
+    """
+    TODO Docs
+    """
+
+    class ProjectStatus(models.TextChoices):
+        """
+        TODO Docs
+        """
+        COMPLETE = 'CO', _('Complete')
+        IN_PROGRESS = 'IP', _('In Progress')
+        PLANNED = 'PL', _('Planned')
+        OTHER = 'OT', _('Other')
+        # TODO
+
+    name = models.CharField(
+        max_length=5,
+        # TODO
+    )
+    authors = models.JSONField(
+        default=list,
+        validators=[JSONSchemaValidator(limit_value=PROJECT_AUTHORS_FIELD_JSON_SCHEMA)],
+        # TODO
+    )
+    description = models.TextField(
+        # TODO
+    )
+    image = models.ImageField(
+        # TODO
+    )
+    url = models.URLField(
+        # TODO
+    )
+    status = models.CharField(
+        max_length=2,
+        choices=ProjectStatus.choices,
+        # TODO
+    )
+    modified = models.DateTimeField(
+        auto_now=True,
+        # TODO
+    )
---
+++ backend/apps/projects/tests/model.py
@@ -0,0 +1,8 @@
+from django.test import TestCase
+
+# Create your model tests here.
+'''
+class TestProjectModel(TestCase):
+    def test_model_*():
+        self.assertEqual(...)
+'''
