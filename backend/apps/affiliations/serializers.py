ab9545e806e11b535dfccda563bf3920632a14f4
epm0dev <epmancini@live.com>
2021-04-11 15:50:44 -0400

    Create a serializer and viewset for the Affiliate model, document serializers.py and views.py modules of the affiliations app, register Affiliate model viewset with the API

--- backend/apps/affiliations/serializers.py
+++ backend/apps/affiliations/serializers.py
@@ -1,5 +1,43 @@
+"""This module contains Django Rest Framework serializers for affiliations application models."""
 from rest_framework import serializers
 
-# from apps.affiliations.models import ...
+from apps.affiliations.models import Affiliate
 
-# Create your serializers here.
+
+class AffiliateSerializer(serializers.ModelSerializer):
+    """A simple Django Rest Framework serializer for the Affiliate model.
+
+    The serialized representation of an affiliate model instance includes the instance's ``name`` and ``website``
+    attributes, as well as the relative URL from which the affiliate's logo can be requested.
+
+    Attributes:  # noqa
+        logo_url: A serializer method field that is included in the serialized representation of an Affiliate model
+        instance rather than the instance's ``logo`` attribute itself.
+    """
+    logo_url = serializers.SerializerMethodField(read_only=True)
+
+    def get_logo_url(self, obj):
+        """A get method for the AffiliateSerializer class' ``logo_url`` attribute.
+
+        This method defines the logic that is evaluated when serializing an Affiliate model instance and retrieving the
+        logo_url attribute. It simply returns the relative URL of the FieldFile instance that is stored in the Affiliate
+        class' ``logo`` attribute.
+
+        Args:
+            obj: The instance of the Affiliate model class that is being serialized.
+
+        Returns:
+            The relative URL where the affiliate's logo can be requested from.
+        """
+        return obj.logo.url
+
+    class Meta:
+        """A class which defines basic configuration options for the AffiliateSerializer class.
+
+        Attributes:  # noqa
+            model: The model class that the AffiliateSerializer class serializes.
+
+            fields: A list of the fields to include in the serialized representation of an Affiliate model instance.
+        """
+        model = Affiliate
+        fields = ['name', 'logo_url', 'website']
--- backend/apps/affiliations/views.py
+++ backend/apps/affiliations/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+"""This module contains Django Rest Framework viewsets for affiliations application models."""
+from rest_framework import viewsets
 
-# Create your views here.
+from apps.affiliations.serializers import AffiliateSerializer
+from apps.affiliations.models import Affiliate
+
+
+class AffiliateViewSet(viewsets.ModelViewSet):
+    """A simple Django Rest Framework viewset, which acts as an API endpoint for the Affiliate model.
+
+    Attributes:  # noqa
+        serializer_class: The ModelSerializer class that serializes objects in the class' ``queryset`` attribute for
+        retrieval via the API endpoint.
+
+        queryset: A queryset containing all of the objects that can be accessed from the
+    """
+    serializer_class = AffiliateSerializer
+    queryset = Affiliate.objects.all()
