4eebac9dd4cc166c790cb2504b45eaa9055fa229
epm0dev <epmancini@live.com>
2021-04-11 12:02:49 -0400

    Document the core.validators module

--- backend/core/validators.py
+++ backend/core/validators.py
@@ -1,3 +1,4 @@
+"""This module contains custom Django validator(s) for cleaning model fields."""
 from django.core.validators import BaseValidator
 from django.core.exceptions import ValidationError as DjangoValidationError
 
@@ -6,14 +7,36 @@ from jsonschema.exceptions import ValidationError as JSONSchemaValidationError
 
 
 class JSONSchemaValidator(BaseValidator):
-    """
-    TODO Docs
-    https://stackoverflow.com/questions/37642742/django-postgresql-json-field-schema-validation
+    """A generic validator for JSONFields which are required to follow a pre-defined JSON schema.
+
+    For example, if you have defined a valid JSON schema named ``MY_JSON_SCHEMA``, and you have a model with a JSONField
+    that you would like to adhere to that schema, set the ``validators`` option for the field as follows:
+
+    ``validators=[JSONSchemaValidator(limit_value=MY_JSON_SCHEMA)]``
+
+    Then, the fields of an instance of your model are cleaned, the ``compare(self, value, schema)`` method below will
+    attempt to validate the value of the JSONField against the provided schema. See https://json-schema.org/ for more
+    information regarding JSON schemas in general.
+
+    Note: This implementation was adapted from the following StackOverflow answer: https://stackoverflow.com/a/49036841
     """
 
     def compare(self, value, schema):
-        """
-        TODO Docs
+        """The method which compares the current value of a JSONField with the provided JSON schema for validation.
+
+        First, an attempt is made to validate the specified value against the provided JSON schema. If this operation
+        succeeds, the value was successfully validated, so no additional work is needed. Otherwise, the value was not
+        successfully validated and a ValidationError is raised by the jsonschema.validate() method. Although the error
+        has the same name as the built-in Django ValidationError, simply allowing it to propagate to the caller of this
+        can cause issues, such as 500 errors, in the frontend, so the jsonschema ValidationError is caught and a Django
+        ValidationError is raised instead.
+
+        Args:
+            value: The value to validate.
+            schema: The JSON schema to validate the specified value against.
+
+        Raises:
+            ValidationError: The value does not follow the provided JSON schema.
         """
         try:
             validate(value, schema)
