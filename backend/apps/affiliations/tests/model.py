afd099bb86dee9c018bcb8b3b49e86e3d9dbd709
epm0dev <epmancini@live.com>
2021-04-12 22:01:45 -0400

    Create Affiliate unit test class skeleton

--- backend/apps/affiliations/tests/model.py
+++ backend/apps/affiliations/tests/model.py
@@ -1,4 +1,24 @@
 """This module contains unit tests for the affiliations application's Django models."""
-from django.test import TestCase
+from core.testcases import VerboseTestCase
 
-# Create your model tests here.
+
+class TestAffiliateModel(VerboseTestCase):
+    """TODO Docs
+    """
+
+    message = 'Testing Affiliate model...'
+
+    def test_logo_path_properly_assigned(self):
+        """TODO Docs
+        """
+        self.assertTrue(False)
+
+    def test_logo_available_at_url(self):
+        """TODO Docs
+        """
+        pass
+
+    def test_affiliate_string_representation(self):
+        """TODO Docs
+        """
+        pass
