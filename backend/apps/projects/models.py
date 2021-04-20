4e4efe9acec66e1c57d87c907ca3023784397183
epm0dev <epmancini@live.com>
2021-04-20 18:39:17 -0400

    Add field options to Project model fields, refactor JSON schema for authors field, make new database migrations, implement tests for authors field schema validation

--- backend/apps/projects/models.py
+++ backend/apps/projects/models.py
@@ -8,14 +8,41 @@ from core.validators import JSONSchemaValidator
 PROJECT_AUTHORS_FIELD_JSON_SCHEMA = {
     'schema': 'http://json-schema.org/draft-07/schema#',
     'title': 'Project Authors',
-    'description': 'A list of authors\' names (as strings) of a project event.',
+    'description': 'A list of authors\' names (as strings) of a group project.',
     'type': 'array',
-    'properties': {
-        'items': {
-            'type': 'string'
-        }
+    'minItems': 1,
+    'items': {
+        'type': 'string',
+        'pattern': '(^(?!\\s*)$)|(^.*\\S.*$)'
     }
 }
+
+# TODO Docs
+#  Define the base file path, which will be appended to the MEDIA_ROOT directory, where affiliate logos should be saved.
+BASE_IMAGE_PATH = 'projects/'
+
+
+def image_path(instance, filename):
+    """TODO Docs
+
+    This callable is used by the Affiliate class' logo field to evaluate the path at which to save a logo.
+
+    Args:
+        instance: An instance of the Affiliate class, from which the name of the affiliate is used to build the final
+            path for the logo.
+        filename: The name of the file, from which the file extension is used to build the final path for the logo.
+
+    Returns:
+        The path where the uploaded affiliate logo should be saved.
+    """
+    # Build the name of the logo file, replacing the following invalid characters: `/` and `\0`.
+    name = instance.name.lower() \
+                        .replace(' ', '_') \
+                        .replace('/', '') \
+                        .replace('\0', '')
+    extension = filename[filename.rindex('.'):]  # Includes the `.`
+
+    return f'{BASE_IMAGE_PATH}{name}/main_image{extension}'
 
 
 class Project(models.Model):
@@ -34,29 +61,68 @@ class Project(models.Model):
         # TODO
 
     name = models.CharField(
-        max_length=5,
-        # TODO
+        max_length=250,
+        null=False,
+        blank=False,
+        default='',
+        editable=True,
+        unique=True,
+        verbose_name='Project Title'
     )
     authors = models.JSONField(
         default=list,
         validators=[JSONSchemaValidator(limit_value=PROJECT_AUTHORS_FIELD_JSON_SCHEMA)],
-        # TODO
+        null=False,
+        blank=False,
+        editable=True,
+        unique=False,
+        verbose_name='Project Authors'
     )
     description = models.TextField(
-        # TODO
+        null=False,
+        blank=False,
+        default='',
+        editable=True,
+        unique=False,
+        verbose_name='Project Description'
     )
     image = models.ImageField(
-        # TODO
+        null=True,
+        blank=True,
+        editable=True,
+        unique=True,
+        verbose_name='Project Image',
+        upload_to=image_path
     )
     url = models.URLField(
-        # TODO
+        null=True,
+        blank=True,
+        editable=True,
+        unique=True,
+        verbose_name='External Project URL',
+        max_length=200
     )
     status = models.CharField(
         max_length=2,
         choices=ProjectStatus.choices,
-        # TODO
+        default=ProjectStatus.PLANNED,
+        null=False,
+        blank=False,
+        editable=True,
+        unique=False,
+        verbose_name='Project Status or Phase'
     )
     modified = models.DateTimeField(
         auto_now=True,
-        # TODO
+        null=False,
+        blank=True,
+        editable=False,
+        unique=False,
+        verbose_name='Date and Time of Last Edit'
     )
+
+    def __str__(self):
+        """TODO Docs
+        """
+        # TODO Implementation
+        pass
--- backend/apps/projects/tests/model.py
+++ backend/apps/projects/tests/model.py
@@ -1,8 +1,77 @@
-from django.test import TestCase
+from django.core.exceptions import ValidationError
+from django.test import tag
 
-# Create your model tests here.
-'''
-class TestProjectModel(TestCase):
-    def test_model_*():
-        self.assertEqual(...)
-'''
+from apps.projects.models import Project
+from core.testcases import VerboseTestCase, Tags
+
+
+class TestProjectModel(VerboseTestCase):
+    """TODO Docs
+    """
+
+    message = 'Testing Project model...'
+
+    @tag(Tags.JSON)
+    def test_valid_authors_one_author(self):
+        """TODO Docs
+        """
+        project = Project(
+            name='Test Project',
+            authors=['Author 1'],
+            description='Project description.',
+            status=Project.ProjectStatus.PLANNED
+        )
+
+        self.assertNotRaises(ValidationError, project.full_clean)
+
+    @tag(Tags.JSON)
+    def test_valid_authors_two_authors(self):
+        """TODO Docs
+        """
+        project = Project(
+            name='Test Project',
+            authors=['Author 1', 'Author 2'],
+            description='Project description.',
+            status=Project.ProjectStatus.PLANNED
+        )
+
+        self.assertNotRaises(ValidationError, project.full_clean)
+
+    @tag(Tags.JSON)
+    def test_invalid_authors_empty_list(self):
+        """TODO Docs
+        """
+        project = Project(
+            name='Test Project',
+            authors=[],
+            description='Project description.',
+            status=Project.ProjectStatus.PLANNED
+        )
+
+        self.assertRaises(ValidationError, project.full_clean)
+
+    @tag(Tags.JSON)
+    def test_invalid_authors_one_valid_one_invalid(self):
+        """TODO Docs
+        """
+        project = Project(
+            name='Test Project',
+            authors=['Author 1', ' '],
+            description='Project description.',
+            status=Project.ProjectStatus.PLANNED
+        )
+
+        self.assertRaises(ValidationError, project.full_clean)
+
+    @tag(Tags.JSON)
+    def test_invalid_authors_two_invalid(self):
+        """TODO Docs
+        """
+        project = Project(
+            name='Test Project',
+            authors=['', ' '],
+            description='Project description.',
+            status=Project.ProjectStatus.PLANNED
+        )
+
+        self.assertRaises(ValidationError, project.full_clean)
