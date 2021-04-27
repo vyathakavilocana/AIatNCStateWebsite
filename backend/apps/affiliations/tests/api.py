4bc5b6913975d8c36be49cc96d1bfe4715969b84
epm0dev <epmancini@live.com>
2021-04-27 10:05:05 -0400

    Implement Affiliate model and API endpoint test cases

--- backend/apps/affiliations/tests/api.py
+++ backend/apps/affiliations/tests/api.py
@@ -1,23 +1,68 @@
 """This module contains unit tests for the affiliations application's API serializers and viewsets."""
-from django.urls import include, path, reverse
-from rest_framework.test import APITestCase, URLPatternsTestCase
+from django.urls import reverse
+from django.test import tag
+from rest_framework import status
+
+from apps.affiliations.models import Affiliate
+from core.testcases import VerboseAPITestCase, Tags
 
 
-# Create your API test cases here.
-'''
-e.g.,
+class AffiliateEndpointTestCase(VerboseAPITestCase):
+    """A test case class which contains unit tests for the Affiliate model API endpoint.
+    """
+    message = 'Testing Affiliate model API endpoint...'
 
-class AccountTests(APITestCase, URLPatternsTestCase):
-    urlpatterns = [
-        path('api/', include('api.urls')),
-    ]
-
-    def test_create_account(self):
+    @classmethod
+    def setUpTestData(cls):
+        """Set up the test data for the test case once when the test case class is being prepared to run.
         """
-        Ensure we can create a new account object.
+        from django.core.files.uploadedfile import SimpleUploadedFile
+        image = SimpleUploadedFile('logo.png', b'file_content')
+
+        cls.affiliate = Affiliate(
+            name='Django Software Foundation',
+            logo=image,
+            website='https://www.djangoproject.com/foundation/',
+        )
+        cls.affiliate.save()
+
+    @classmethod
+    def tearDownClass(cls):
+        """Delete the Affiliate model instance used for testing once all the test cases have been executed.
+
+        This is necessary to ensure that the temporary image file created on the disk for the tests is deleted.
         """
-        url = reverse('account-list')
-        response = self.client.get(url, format='json')
+        super().tearDownClass()
+        if hasattr(cls, 'affiliate'):
+            cls.affiliate.delete()
+
+    # noinspection DuplicatedCode
+    @tag(Tags.API)
+    def test_read_only(self):
+        """Ensure that the API endpoint for multiple Affiliate model instances is read-only.
+        """
+        url = reverse('affiliate-list')
+
+        response = self.client.post(url, data={})
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+        response = self.client.put(url, data={})
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+        response = self.client.patch(url, data={})
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+        response = self.client.get(f'{url}/{self.affiliate.pk}/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(len(response.data), 1)
-'''
+
+    @tag(Tags.API)
+    def test_logo_url_serializer_field(self):
+        """Ensure that the `logo_url` serializer method field has the correct value in an API response.
+        """
+        url = reverse('affiliate-list')
+        response = self.client.get(f'{url}/{self.affiliate.pk}/')
+
+        self.assertEqual(self.affiliate.logo.url, response.data['logo_url'])
--- backend/apps/affiliations/tests/model.py
+++ backend/apps/affiliations/tests/model.py
@@ -1,24 +1,76 @@
 """This module contains unit tests for the affiliations application's Django models."""
-from core.testcases import VerboseTestCase
+from django.conf import settings
+from django.test import tag
+import os
+
+from apps.affiliations.models import Affiliate, BASE_LOGO_PATH
+from core.testcases import VerboseTestCase, Tags
 
 
 class TestAffiliateModel(VerboseTestCase):
-    """TODO Docs
+    """A test case class which contains unit tests for Affiliate model functionality.
     """
-
     message = 'Testing Affiliate model...'
 
+    @classmethod
+    def setUpTestData(cls):
+        """Creates a generic image for creating Affiliate objects in individual tests.
+        """
+        from django.core.files.uploadedfile import SimpleUploadedFile
+
+        cls.image = SimpleUploadedFile('logo.png', b'file_content')
+
+    @tag(Tags.MODEL)
+    def test_logo_file_deleted_with_object(self):
+        """Ensure that an affiliate's logo is deleted from the disk when its associated Affiliate object is deleted.
+        """
+        affiliate = Affiliate(
+            name='Django Software Foundation',
+            logo=self.image,
+            website='https://www.djangoproject.com/foundation/',
+        )
+        affiliate.save()
+
+        path = f'{affiliate.logo.path}'
+        affiliate.delete()
+
+        if os.path.isfile(path):
+            self.fail('Logo image file was not deleted along with the model instance.')
+
+    @tag(Tags.MODEL)
     def test_logo_path_properly_assigned(self):
-        """TODO Docs
+        """Ensure that the URL and name of an affiliate's logo are properly assigned when creating an Affiliate object.
         """
-        self.assertTrue(False)
+        affiliate = Affiliate(
+            name='JetBrains',
+            logo=self.image,
+            website='https://www.python.org/psf-landing/',
+        )
+        affiliate.save()
 
-    def test_logo_available_at_url(self):
-        """TODO Docs
-        """
-        pass
+        try:
+            self.assertEqual(affiliate.logo.url, f'{settings.MEDIA_URL}{BASE_LOGO_PATH}jetbrains.png')
+        except AssertionError as e:
+            affiliate.delete()
+            self.fail(e)
 
+        affiliate.delete()
+
+    @tag(Tags.MODEL)
     def test_affiliate_string_representation(self):
-        """TODO Docs
+        """Ensure that the string representation of an affiliate instance simply contains the affiliate's name as-is.
         """
-        pass
+        affiliate = Affiliate(
+            name='Google',
+            logo=self.image,
+            website='https://www.google.com',
+        )
+        affiliate.save()
+
+        try:
+            self.assertEqual(str(affiliate), 'Google')
+        except AssertionError as e:
+            affiliate.delete()
+            self.fail(e)
+
+        affiliate.delete()
