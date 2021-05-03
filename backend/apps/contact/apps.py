93c5b235355735f59a10c8fac9f05ba54298d4a3
epm0dev <epmancini@live.com>
2021-05-02 20:05:08 -0400

    Document contact.apps module

--- backend/apps/contact/apps.py
+++ backend/apps/contact/apps.py
@@ -1,5 +1,12 @@
+"""This module contains a simple configuration for the contact Django application."""
 from django.apps import AppConfig
 
 
 class ContactConfig(AppConfig):
+    """A simple Django application configuration class which only contains the name for the contact Django app.
+
+    Attributes:  # noqa
+        name: A string containing the name of the contact app. The same name is included in the config.settings module's
+        list of installed apps, ``INSTALLED_APPS``, so that this application is properly registered by Django.
+    """
     name = 'contact'
