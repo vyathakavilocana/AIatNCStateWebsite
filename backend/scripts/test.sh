246036e48862ffd761bfb9c352dd5fe9c7451179
epm0dev <epmancini@live.com>
2021-05-03 11:40:28 -0400

    Invert logic for executing `coverage html` command in backend testing script

--- backend/scripts/test.sh
+++ backend/scripts/test.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+﻿#!/usr/bin/env bash
 
 cd /app || exit
 
@@ -6,7 +6,7 @@ echo -e "\e[95mRunning Unit Tests...\e[39m"
 coverage run manage.py test
 echo -e "\e[95mDone\e[39m"
 
-if [ "$1" = '--no-html' ] || [ "$1" = '-nh' ]; then
+if [ "$1" != '--no-html' ] && [ "$1" != '-nh' ]; then
   echo -e "\e[95mGenerating HTML Coverage Report\e[39m"
   coverage html
   echo -e "\e[95mDone\e[39m"
