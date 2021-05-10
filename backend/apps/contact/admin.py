4bdfc459204ae5a8a43c77a3e0958a1c62ec9ab6
epm0dev <epmancini@live.com>
2021-05-10 12:31:02 -0400

    Implement contact form models

--- backend/apps/contact/admin.py
+++ backend/apps/contact/admin.py
@@ -1,3 +1,58 @@
+"""TODO Docs"""
 from django.contrib import admin
+from polymorphic.admin import PolymorphicChildModelAdmin, PolymorphicParentModelAdmin, PolymorphicChildModelFilter
 
-# Register your models here.
+from apps.contact.models import (
+    ContactFormBase, GuestSpeakerContactForm, MentorContactForm, EventOrganizerContactForm, PartnerContactForm
+)
+
+
+class ContactFormBaseChildAdmin(PolymorphicChildModelAdmin):
+    """TODO Docs
+    """
+    base_model = ContactFormBase
+
+    # TODO ?
+    """
+    base_form = ...
+    base_fieldsets = (
+        ...
+    )
+    """
+
+
+@admin.register(GuestSpeakerContactForm)
+class GuestSpeakerContactFormAdmin(ContactFormBaseChildAdmin):
+    """TODO Docs
+    """
+    base_model = GuestSpeakerContactForm
+
+
+@admin.register(MentorContactForm)
+class MentorContactFormFormAdmin(ContactFormBaseChildAdmin):
+    """TODO Docs
+    """
+    base_model = MentorContactForm
+
+
+@admin.register(EventOrganizerContactForm)
+class EventOrganizerContactFormAdmin(ContactFormBaseChildAdmin):
+    """TODO Docs
+    """
+    base_model = EventOrganizerContactForm
+
+
+@admin.register(PartnerContactForm)
+class PartnerContactFormAdmin(ContactFormBaseChildAdmin):
+    """TODO Docs
+    """
+    base_model = PartnerContactForm
+
+
+@admin.register(ContactFormBase)
+class ContactFormBaseParentAdmin(PolymorphicParentModelAdmin):
+    """TODO Docs
+    """
+    base_model = ContactFormBase
+    child_models = (GuestSpeakerContactForm, MentorContactForm, EventOrganizerContactForm, PartnerContactForm)
+    list_filter = (PolymorphicChildModelFilter,)
--- backend/apps/contact/models.py
+++ backend/apps/contact/models.py
@@ -1,4 +1,387 @@
+"""TODO Docs"""
+from django.contrib.contenttypes.fields import GenericForeignKey, GenericRelation
+from django.contrib.contenttypes.models import ContentType
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+from polymorphic.models import PolymorphicModel
+
+from core.validators import JSONSchemaValidator
+
+GUEST_SPEAKER_AVAILABILITY_FIELD_SCHEMA = {
+    'schema': 'http://json-schema.org/draft-07/schema#',
+    'title': 'Availability',
+    'type': 'array',
+    'maxItems': 3,
+    'items': {
+        'title': 'Date/Time of Availability',
+    }
+}
+
+MENTOR_MEETING_INFORMATION_FIELD_SCHEMA = {
+    'schema': 'http://json-schema.org/draft-07/schema#',
+    'title': 'Weekly Meeting Availability',
+    'type': 'array',
+    'minItems': 1,
+    'items': {
+        'title': 'Weekday/Time of Availability',
+        'type': 'object',
+        'properties': {
+            'weekday': {
+                'title': 'Day of Week',
+                'type': 'string',
+                'enum': ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+            },
+            'time': {
+                'title': 'Time Available',
+                'type': 'string',
+                'format': 'time'
+            }
+        },
+        'required': ['weekday', 'time'],
+        'additionalProperties': False
+    }
+}
 
 
-# Create your models here.
+class ContactFormBase(PolymorphicModel):
+    """TODO Docs
+    """
+    first_name = models.CharField(
+        max_length=80,
+        null=False,
+        blank=False,
+        default='',
+        editable=False,
+        unique=False,
+        verbose_name='First Name',
+    )
+    last_name = models.CharField(
+        max_length=80,
+        null=False,
+        blank=False,
+        default='',
+        editable=False,
+        unique=False,
+        verbose_name='Last Name',
+    )
+    contacts = GenericRelation('core.ContactInfo')
+    affiliation = models.CharField(
+        max_length=150,
+        null=True,
+        blank=True,
+        default='',
+        editable=False,
+        unique=False,
+        verbose_name='Affiliate Organization',
+    )
+    thoughts = models.TextField(
+        null=True,
+        blank=True,
+        default='',
+        editable=False,
+        unique=False,
+        verbose_name='Additional Thoughts/Questions/Concerns',
+    )
+    reviewed = models.BooleanField(
+        default=False,
+        editable=False,
+        verbose_name='Form Reviewed?',
+    )
+    ignored = models.BooleanField(
+        default=False,
+        editable=False,
+        verbose_name='Ignored?',
+    )
+    comments = GenericRelation('contact.AdminComment')
+
+
+class GuestSpeakerContactForm(ContactFormBase):
+    """TODO Docs
+    """
+    topic = models.CharField(
+        max_length=250,
+        null=False,
+        blank=False,
+        default='',
+        editable=False,
+        unique=False,
+        verbose_name='Presentation Topic',
+    )
+    availability = models.JSONField(
+        default=list,
+        validators=[JSONSchemaValidator(limit_value=GUEST_SPEAKER_AVAILABILITY_FIELD_SCHEMA)],
+        null=False,
+        blank=False,
+        editable=False,
+        unique=False,
+        verbose_name='Availability',
+    )
+    length = models.PositiveSmallIntegerField(
+        null=False,
+        blank=False,
+        default=1,
+        editable=False,
+        unique=False,
+        verbose_name='Presentation Length',
+    )
+    visual_aids = models.CharField(
+        max_length=300,
+        null=True,
+        blank=True,
+        default='',
+        editable=False,
+        unique=False,
+        verbose_name='Prepared Visual Aids',
+    )
+    addl_visual_aids = models.CharField(
+        max_length=300,
+        null=True,
+        blank=True,
+        default='',
+        editable=False,
+        unique=False,
+        verbose_name='Additional Visual Aids Required',
+    )
+    addl_tech = models.CharField(
+        max_length=300,
+        null=True,
+        blank=True,
+        default='',
+        editable=False,
+        unique=False,
+        verbose_name='Additional Tech Setup Required',
+    )
+    consent_audio_rec = models.BooleanField(
+        default=False,
+        editable=False,
+        verbose_name='Consent to Record Audio',
+    )
+    consent_video_rec = models.BooleanField(
+        default=False,
+        editable=False,
+        verbose_name='Consent to Record Video',
+    )
+    consent_streaming = models.BooleanField(
+        default=False,
+        editable=False,
+        verbose_name='Consent to Upload Recordings to Streaming Platform(s)',
+    )
+    consent_materials = models.BooleanField(
+        default=False,
+        editable=False,
+        verbose_name='Consent to Share Presentation Materials With Club Members',
+    )
+
+
+class MentorContactForm(ContactFormBase):
+    """TODO Docs
+    """
+    students = models.PositiveSmallIntegerField(
+        null=False,
+        blank=False,
+        default=1,
+        editable=False,
+        unique=False,
+        verbose_name='Number of Students to Mentor',
+        validators=[
+            MinValueValidator(1, 'Number of students too small.'),
+            MaxValueValidator(6, 'Number of students too large.'),
+        ],
+    )
+    field_type = models.CharField(
+        max_length=60,
+        null=False,
+        blank=False,
+        default='',
+        editable=False,
+        unique=False,
+        verbose_name='Type of Field',
+    )
+    field_name = models.CharField(
+        max_length=200,
+        null=False,
+        blank=False,
+        default='',
+        editable=False,
+        unique=False,
+        verbose_name='Field/Sector Name',
+    )
+    field_description = models.TextField(
+        null=True,
+        blank=True,
+        default='',
+        editable=False,
+        unique=False,
+        verbose_name='Field/Sector Name',
+    )
+    availability_start = models.DateField(
+        null=False,
+        blank=False,
+        editable=False,
+        unique=False,
+        verbose_name='Start of Mentorship Availability',
+    )
+    availability_end = models.DateField(
+        null=True,
+        blank=True,
+        editable=False,
+        unique=False,
+        verbose_name='End of Mentorship Availability',
+    )
+    meeting_information = models.JSONField(
+        default=list,
+        validators=[JSONSchemaValidator(limit_value=MENTOR_MEETING_INFORMATION_FIELD_SCHEMA)],
+        null=False,
+        blank=False,
+        editable=False,
+        unique=False,
+        verbose_name='Weekly Meeting Availability',
+    )
+    weekly_minutes = models.PositiveSmallIntegerField(
+        null=False,
+        blank=False,
+        default=1,
+        editable=False,
+        unique=False,
+        verbose_name='Minutes of Availability Per Week',
+        validators=[
+            MinValueValidator(1, 'Number of minutes too small.'),
+        ],
+    )
+
+
+class EventOrganizerContactForm(ContactFormBase):
+    """TODO Docs
+
+    TODO Validation for min/max attendees
+    """
+    event_type = models.CharField(
+        max_length=120,
+        null=False,
+        blank=False,
+        default='',
+        editable=False,
+        unique=False,
+        verbose_name='Type of Event to Organize',
+    )
+    financial_assistance = models.BooleanField(
+        default=False,
+        editable=False,
+        verbose_name='Financial Assistance from Club Required?',
+    )
+    min_attendees = models.PositiveSmallIntegerField(
+        null=True,
+        blank=False,
+        default=1,
+        editable=False,
+        unique=False,
+        verbose_name='Estimated/Desired Minimum Number of Attendees',
+    )
+    max_attendees = models.PositiveSmallIntegerField(
+        null=True,
+        blank=False,
+        default=1,
+        editable=False,
+        unique=False,
+        verbose_name='Estimated/Desired Maximum Number of Attendees',
+    )
+    advertising = models.TextField(
+        null=False,
+        blank=False,
+        default='',
+        editable=False,
+        unique=False,
+        verbose_name='Event Advertising Campaign Information',
+    )
+
+
+class PartnerContactForm(ContactFormBase):
+    """TODO Docs
+
+    TODO Validation for min/max org_size.
+    """
+    commercial = models.BooleanField(
+        default=False,
+        editable=False,
+        verbose_name='Commercial Organization?',
+    )
+    industry = models.CharField(
+        max_length=300,
+        null=True,
+        blank=True,
+        default='',
+        editable=False,
+        unique=False,
+        verbose_name='Industries',
+    )
+    min_org_size = models.PositiveSmallIntegerField(
+        null=True,
+        blank=False,
+        default=1,
+        editable=False,
+        unique=False,
+        verbose_name='Estimated Minimum Size of Organization',
+    )
+    max_org_size = models.PositiveSmallIntegerField(
+        null=True,
+        blank=False,
+        default=1,
+        editable=False,
+        unique=False,
+        verbose_name='Estimated Maximum Size of Organization',
+    )
+    funding = models.BooleanField(
+        default=False,
+        editable=False,
+        verbose_name='Willing/Able to Provide Funding for Club Initiatives',
+    )
+    initiatives = models.TextField(
+        null=True,
+        blank=True,
+        default='',
+        editable=False,
+        unique=False,
+        verbose_name='Club Initiatives to Provide Funding For',
+    )
+
+
+class AdminComment(models.Model):
+    """TODO Docs
+
+    # TODO Validation to ensure non-admin users can't create admin comments.
+    # TODO Validation to ensure that form_type is actually one of the contact form models.
+    """
+    first_name = models.CharField(
+        max_length=80,
+        null=False,
+        blank=False,
+        default='',
+        editable=False,
+        unique=False,
+        verbose_name='First Name',
+    )
+    last_name = models.CharField(
+        max_length=80,
+        null=False,
+        blank=False,
+        default='',
+        editable=False,
+        unique=False,
+        verbose_name='Last Name',
+    )
+    created = models.DateTimeField(
+        auto_now_add=True,
+        null=False,
+        blank=True,
+        editable=False,
+        unique=False,
+        verbose_name='Time/Date Comment Added'
+    )
+
+    form_type = models.ForeignKey(
+        ContentType,
+        on_delete=models.CASCADE,
+        related_query_name='comments'
+    )
+    form_id = models.PositiveIntegerField()
+    form = GenericForeignKey('form_type', 'form_id')
