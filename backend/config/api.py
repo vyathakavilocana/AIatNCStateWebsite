45ea225c867b9f928aad3471bbeb571b2462955c
epm0dev <epmancini@live.com>
2021-05-24 17:48:51 -0400

    Disable 'users' API endpoint

--- backend/config/api.py
+++ backend/config/api.py
@@ -1,7 +1,7 @@
 """This module contains configuration for the API. Endpoints are registered with a Django Rest Framework router."""
 from rest_framework import routers
 
-from apps.users.views import UserViewSet
+# from apps.users.views import UserViewSet
 from apps.affiliations.views import AffiliateViewSet
 from apps.events.views import EventViewSet
 from apps.projects.views import ProjectViewSet
@@ -17,7 +17,7 @@ api = routers.DefaultRouter()
 api.trailing_slash = '/?'
 
 # Register API endpoints
-api.register(r'users', UserViewSet, basename='user')
+# api.register(r'users', UserViewSet, basename='user')
 api.register(r'affiliates', AffiliateViewSet, basename='affiliate')
 api.register(r'events', EventViewSet, basename='event')
 api.register(r'projects', ProjectViewSet, basename='project')
