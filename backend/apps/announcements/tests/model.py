c7fbbffa2bbda08528b83d91f4e25a731b3034ee
epm0dev <epmancin@ncsu.edu>
2022-02-01 13:53:41 -0500

    Add empty Announcement model unit test methods

--- backend/apps/announcements/tests/model.py
+++ backend/apps/announcements/tests/model.py
@@ -1,4 +1,82 @@
 """This module contains unit tests for the announcement application's Django models."""
-from django.test import TestCase
+from django.test import tag
 
-# Create your model tests here.
+from core.testcases import VerboseTestCase, Tags
+from apps.announcements.models import Announcement
+
+
+class TestAnnouncementModel(VerboseTestCase):
+    """TODO Docs
+    """
+    message = 'Testing Announcement model...'
+
+    @tag(Tags.MODEL)
+    def test_str(self):
+        """Ensure that an Announcement object's string representation is simply its title.
+        """
+        announcement = Announcement(
+            title='Title'
+        )
+        announcement.save()
+
+        self.assertEqual(announcement.title, str(announcement))
+
+    @tag(Tags.JSON, Tags.VALIDATION)
+    def test_invalid_body_empty_list(self):
+        """
+        """
+        self.not_implemented()
+
+    @tag(Tags.JSON, Tags.VALIDATION)
+    def test_invalid_body_object_no_props(self):
+        """
+        """
+        self.not_implemented()
+
+    @tag(Tags.JSON, Tags.VALIDATION)
+    def test_invalid_body_valid_and_invalid_object(self):
+        """
+        """
+        self.not_implemented()
+
+    @tag(Tags.JSON, Tags.VALIDATION)
+    def test_valid_body_valid_object(self):
+        """
+        """
+        self.not_implemented()
+
+    @tag(Tags.JSON, Tags.VALIDATION)
+    def test_valid_body_valid_objects(self):
+        """
+        """
+        self.not_implemented()
+
+    @tag(Tags.JSON, Tags.VALIDATION)
+    def test_hr(self):
+        """
+        """
+        self.not_implemented()
+
+    @tag(Tags.JSON, Tags.VALIDATION)
+    def test_p(self):
+        """
+        """
+        self.not_implemented()
+
+    @tag(Tags.JSON, Tags.VALIDATION)
+    def test_img(self):
+        """
+        """
+        self.not_implemented()
+
+    @tag(Tags.JSON, Tags.VALIDATION)
+    def test_h(self):
+        """
+        """
+        self.not_implemented()
+
+    @tag(Tags.JSON, Tags.VALIDATION)
+    def test_a(self):
+        """
+        """
+        self.not_implemented()
